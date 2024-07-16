c900e1068671bb8552731f4c541ada936e09a1aa
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2024-07-16 20:30:41 +0800

    Create nodefetchapi_demo.js

---
+++ nodefetchapi_demo.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const homedir = require('os').homedir();
+const client_id= 'your_id';
+const client_secret= 'your_secret';
+
+const mytool = require("./mymodule");
+const {look_keys,print} = mytool;
+const now = new Date();
+const axios = require('axios');
+const auth_url = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token"
+const paragraph = "=================================================";
+console.clear();
+
+var details =	{
+			grant_type:"client_credentials" ,	
+			client_id:`${client_id}`,
+			client_secret:`${client_secret}`
+			};
+let formBody = [];
+for (var property in details) {
+  var encodedKey = encodeURIComponent(property);
+  var encodedValue = encodeURIComponent(details[property]);
+  formBody.push(encodedKey + "=" + encodedValue);
+}
+formBody = formBody.join("&");
+
+fetch(`${auth_url}`,{
+	method:'POST',
+	headers: { "Accept-Encoding": "br,gzip",'content-type': 'application/x-www-form-urlencoded'},
+	body : formBody
+})
+.then(res=> res.json())
+.then(d=> d.access_token)
+.then(t=> console.log(t))
+.catch(err=>console.log(err))
